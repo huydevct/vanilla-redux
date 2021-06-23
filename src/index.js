05e8e9bf6b8834c961713eb4e64629e7522e8ce9
huydevct <huydevct@gmail.com>
2021-06-24 01:21:05 +0700

    fix sth

--- src/index.js
+++ src/index.js
@@ -46,10 +46,34 @@ minus.addEventListener("click", handleMinus);*/
 /*To do JS */
 
 import { createStore } from "redux";
-
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-let toDos = [];
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch(action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+const onSubmit = (event) => {
+  event.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo});
+}
+
+form.addEventListener("submit", onSubmit);
 
